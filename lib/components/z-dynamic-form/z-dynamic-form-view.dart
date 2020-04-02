abbd54b6ccd2fdb0707d625f30e8448731990e92
Victor <victor.marques@zellar.com.br>
2020-04-02 14:21:33 -0300

    pull

--- lib/components/z-dynamic-form/z-dynamic-form-view.dart
+++ lib/components/z-dynamic-form/z-dynamic-form-view.dart
@@ -4,6 +4,8 @@ import 'package:z_components/components/z-collection-item.dart';
 import 'package:z_components/components/z-collection.dart';
 import 'package:z_components/components/z-dynamic-form/z-dynamic-form.dart';
 import 'package:z_components/components/z-inputs/z-input-cpf.dart';
+import 'package:z_components/components/z-inputs/z-input-data-de-nascimento.dart';
+import 'package:z_components/components/z-inputs/z-input-email.dart';
 import 'package:z_components/components/z-radio-group/z-dynamic-form-type.dart';
 import 'package:z_components/i-view.dart';
 import 'package:z_components/view-model/z-dynamic-form-viewmodel.dart';
@@ -33,6 +35,13 @@ class ZDynamicFormView extends IView<ZDyanmicForm> {
         case ZDynamicFormType.CPF:
           return _buildCPF(x);
           break;
+        case ZDynamicFormType.EMAIL:
+          return _buildEmail(x);
+          break;
+        case ZDynamicFormType.DATE:
+          return _buildDate(x);
+          break;
+
       }
 
       return new Container();
@@ -67,4 +76,25 @@ class ZDynamicFormView extends IView<ZDyanmicForm> {
       onChange: (value) => json[item.nomeCampo] = value,
     );
   }
+
+  Widget _buildEmail(ZDynamicFormViewModel item) {
+    TextEditingController textEditingController = new TextEditingController();
+    FocusNode focusNode = new FocusNode();
+    return new ZInputEmail(
+      emailFocus: focusNode,
+      controllerEmail: textEditingController,
+      onChange: (value) => json[item.nomeCampo] = value,
+    );
+  }
+
+  Widget _buildDate(ZDynamicFormViewModel item) {
+    TextEditingController textEditingController = new TextEditingController();
+    FocusNode focusNode = new FocusNode();
+    return new ZInputDataNascimento(
+      dataFocus: focusNode,
+      controllerData: textEditingController,
+      onChange: (value) => json[item.nomeCampo] = value,
+    );
+  }
+
 }
