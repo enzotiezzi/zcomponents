c7c6de0c70f540b7f5b7cabe3d12c400af849942
Victor <victor.marques@zellar.com.br>
2020-04-02 09:55:06 -0300

    pull

---
+++ lib/view-model/formulario-viewmodel.dart
@@ -0,0 +1,71 @@
+class FormularioViewModel {
+  String idModelo;
+  String idAtributo;
+  String idTipo;
+  String modelo;
+  String label;
+  String descricao;
+  String tipo;
+  int ordem;
+  int tamanhoMaximo;
+  int versao;
+  bool obrigatorio;
+  bool multiplaEscolha;
+  String opcoes;
+  String nomeCampo;
+
+  FormularioViewModel({
+    this.idModelo,
+    this.idAtributo,
+    this.idTipo,
+    this.modelo,
+    this.label,
+    this.descricao,
+    this.tipo,
+    this.ordem,
+    this.tamanhoMaximo,
+    this.versao,
+    this.obrigatorio,
+    this.multiplaEscolha,
+    this.opcoes,
+    this.nomeCampo,
+  });
+
+  factory FormularioViewModel.fromJson(Map<String, dynamic> json) {
+    return new FormularioViewModel(
+      idModelo: json['idModelo'],
+      idAtributo: json['idAtributo'],
+      idTipo: json['idTipo'],
+      modelo: json['modelo'],
+      label: json['label'],
+      descricao: json['descricao'],
+      tipo: json['tipo'],
+      ordem: json['ordem'],
+      tamanhoMaximo: json['tamanhoMaximo'],
+      versao: json['versao'],
+      obrigatorio: json['obrigatorio'],
+      multiplaEscolha: json['multiplaEscolha'],
+      opcoes: json['opcoes'],
+      nomeCampo: json['nomeCampo'],
+    );
+  }
+
+  Map toMap() {
+    return {
+      "idModelo": this.idModelo,
+      "idAtributo": this.idAtributo,
+      "idTipo": this.idTipo,
+      "modelo": this.modelo,
+      "label": this.label,
+      "descricao": this.descricao,
+      "tipo": this.tipo,
+      "ordem": this.ordem,
+      "tamanhoMaximo": this.tamanhoMaximo,
+      "versao": this.versao,
+      "obrigatorio": this.obrigatorio,
+      "multiplaEscolha": this.multiplaEscolha,
+      "opcoes": this.opcoes,
+      "nomeCampo": this.nomeCampo,
+    };
+  }
+}
