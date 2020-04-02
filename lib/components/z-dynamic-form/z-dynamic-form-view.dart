c5abcbc0977e328923c7ad6bfd30f3d707f684d1
Victor <victor.marques@zellar.com.br>
2020-04-02 14:47:26 -0300

    pull

--- lib/components/z-dynamic-form/z-dynamic-form-view.dart
+++ lib/components/z-dynamic-form/z-dynamic-form-view.dart
@@ -6,6 +6,7 @@ import 'package:z_components/components/z-dynamic-form/z-dynamic-form.dart';
 import 'package:z_components/components/z-inputs/z-input-cpf.dart';
 import 'package:z_components/components/z-inputs/z-input-data-de-nascimento.dart';
 import 'package:z_components/components/z-inputs/z-input-email.dart';
+import 'package:z_components/components/z-inputs/z-input-generic.dart';
 import 'package:z_components/components/z-radio-group/z-dynamic-form-type.dart';
 import 'package:z_components/i-view.dart';
 import 'package:z_components/view-model/z-dynamic-form-viewmodel.dart';
@@ -100,6 +101,16 @@ class ZDynamicFormView extends IView<ZDyanmicForm> {
   }
 
   Widget _buildNumber(ZDynamicFormViewModel item){
+    TextEditingController textEditingController = new TextEditingController();
+    FocusNode focusNode = new FocusNode();
+    return new ZInputGeneric(
+        titulo: item.label,
+        inputPadraoFocus: focusNode,
+        controllerInputPadrao: textEditingController,
+        onChange: (value) => json[item.nomeCampo] = value,
+        tipoTeclado: TextInputType.number,
+    );
+
   }
 
 }
