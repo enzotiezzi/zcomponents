5dbfe4a175cd9df469aa104edd7b7f571451254e
giuliano <giuliano.goria@zellar.com.br>
2020-05-18 17:48:56 -0300

    baixar

--- lib/components/confirmar-senha/confirmar-senha.dart
+++ lib/components/confirmar-senha/confirmar-senha.dart
@@ -12,7 +12,8 @@ class ConfirmarSenha {
   String cpf;
   BuildContext context;
 
-  ConfirmarSenha({@required this.token,@required this.cpf, @required this.context});
+  ConfirmarSenha(
+      {@required this.token, @required this.cpf, @required this.context});
 
   Future<bool> dialogConfirmarSenha() {
     return showDialog(
@@ -20,7 +21,8 @@ class ConfirmarSenha {
       barrierDismissible: false,
       builder: (BuildContext context) => ZAlertDialog(
         zDialog: ZDialog.normal,
-        child: new Column(crossAxisAlignment: CrossAxisAlignment.center,
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             new Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -41,44 +43,27 @@ class ConfirmarSenha {
                 )
               ],
             ),
-            new Container(alignment: Alignment.center,
-              padding: const EdgeInsets.all(8.0),
-              child: new Container(alignment: Alignment.center,
-                margin: const EdgeInsets.only(bottom: 8),
-                color: Colors.white,
-                padding: EdgeInsets.only(left: 15.0, top: 8.0),
-                child: new Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    new Container(color: Colors.transparent,height: 1,width: MediaQuery.of(context).size.width * 0.115,),
-                    new Expanded(
-                        child: new Container(alignment: Alignment.center,
-                            margin: EdgeInsets.only(right: 20.0, top: 8),
-                            child: new PinCodeTextField(
-                              highlightColor: const Color(0xff2BBAB4),
-                              highlight: true,
-                              defaultBorderColor: const Color(0xffC7C7CC),
-                              hasTextBorderColor: const Color(0xffC7C7CC),
-                              maxLength: 4,
-                              pinBoxHeight: MainStyle.get(context).sizePin,
-                              pinBoxWidth: MainStyle.get(context).sizePin,
-                              maskCharacter: "*",
-                              pinBoxRadius: 6,
-                              onDone: (text) async {
-                                await _verificarPin(text);
-                              },
-                              pinTextAnimatedSwitcherTransition:
-                                  ProvidedPinBoxTextAnimation.scalingTransition,
-                              pinTextAnimatedSwitcherDuration:
-                                  Duration(milliseconds: 150),
-                              pinTextStyle: new TextStyle(fontSize: 22),
-                              hideCharacter: true,
-                            ))),
-                  ],
-                ),
-              ),
-            ),
+            new PinCodeTextField(
+               highlightColor: const Color(0xff2BBAB4),
+               highlight: true,
+               defaultBorderColor: const Color(0xffC7C7CC),
+               hasTextBorderColor: const Color(0xffC7C7CC),
+               maxLength: 4,
+               pinBoxHeight: MainStyle.get(context).sizePin,
+               pinBoxWidth: MainStyle.get(context).sizePin,
+               maskCharacter: "*",
+               pinBoxRadius: 6,
+               onDone: (text) async {
+                 await _verificarPin(text);
+               },
+               pinTextAnimatedSwitcherTransition:
+               ProvidedPinBoxTextAnimation.scalingTransition,
+               pinTextAnimatedSwitcherDuration:
+               Duration(milliseconds: 150),
+               pinTextStyle: new TextStyle(fontSize: 22),
+               hideCharacter: true,wrapAlignment: WrapAlignment.center,
+             )
+
           ],
         ),
       ),
@@ -86,7 +71,8 @@ class ConfirmarSenha {
   }
 
   Future<void> _verificarPin(String text) {
-    IConfirmarSenhaService iConfirmarSenhaService = new ConfirmarSenhaService(token);
+    IConfirmarSenhaService iConfirmarSenhaService =
+        new ConfirmarSenhaService(token);
 
     ConfirmarSenhaViewModel confimarSenhaViewModel = ConfirmarSenhaViewModel(
       pin: text,
