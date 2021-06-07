e766c728bf620d072c865c9dfaf942bbf8068997
Giuliano Ortiz <giu.oortiz@gmail.com>
2021-06-07 13:21:01 -0300

    componente de aba

--- example/lib/tela-teste-card.dart
+++ example/lib/tela-teste-card.dart
@@ -10,6 +10,8 @@ import 'package:z_components/components/fluxo-admin/detalhe-usuario.dart';
 import 'package:z_components/view-model/usuario-adm-viewmodel.dart';
 import 'package:z_components/view-model/app-view-model.dart';
 import 'package:brasil_fields/brasil_fields.dart';
+import 'package:z_components/components/z-aba/z-aba.dart';
+import 'package:z_components/components/z-aba/z-aba-viewmodel.dart';
 
 class TelaTesteCard extends StatefulWidget {
   @override
@@ -23,14 +25,12 @@ class _TelaTesteCardState extends State<TelaTesteCard> {
       appBar: new AppBar(
         centerTitle: true,
         title: new Container(
-
           child: new Text(
             "Teste dos cards",
             style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
           ),
         ),
       ),
-
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -42,12 +42,14 @@ class _TelaTesteCardState extends State<TelaTesteCard> {
                 nomeUsuario: "Luiz Fernando",
                 status: "Ativo",
                 telefone: "(11)961479207",
-                onTap: (){},
+                onTap: () {},
                 quantidadeApps: "3",
               ),
             ),
-            SizedBox(height: 10,),
-        /*    ZItemTileModuloAdm(
+            SizedBox(
+              height: 10,
+            ),
+            /*    ZItemTileModuloAdm(
               onTap: (){
                 Navigator.push(context, MaterialPageRoute(
                     builder: (context)=>DetalheModulo(
@@ -69,22 +71,25 @@ class _TelaTesteCardState extends State<TelaTesteCard> {
               dataVinculo: "01/09/2018",
             ),*/
             ZItemTileModuloGestao(
-              onTap: (){
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context)=>
-                        DetalheUsuario(
-                          editarDados: false,
-                          cliqueEditar: false,
-                          appUsuarioContaViewModel: new AppUsuarioContaViewModel(
-                            status: "Ativo",
-                            dataVinculo: (null != null)
-                                ? UtilData.obterDataDDMMAAAA(DateTime.parse(null))
-                                : "Nunca",
-                            dataInativacao: "",
-                            app: new AppViewModel(nome: "Teste"),
-                            usuario: new UsuarioAdmViewModel(nome: "Teste")
-                          ),
-                        )));
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => DetalheUsuario(
+                              editarDados: false,
+                              cliqueEditar: false,
+                              appUsuarioContaViewModel:
+                                  new AppUsuarioContaViewModel(
+                                      status: "Ativo",
+                                      dataVinculo: (null != null)
+                                          ? UtilData.obterDataDDMMAAAA(
+                                              DateTime.parse(null))
+                                          : "Nunca",
+                                      dataInativacao: "",
+                                      app: new AppViewModel(nome: "Teste"),
+                                      usuario: new UsuarioAdmViewModel(
+                                          nome: "Teste")),
+                            )));
               },
               nomeModulo: "Quadro pessoal",
               status: "Ativo",
@@ -94,8 +99,36 @@ class _TelaTesteCardState extends State<TelaTesteCard> {
               themeData: Theme.of(context),
               cpfFocus: new FocusNode(),
               controllerCpf: new TextEditingController(),
-              validacao: (b){},
-
+              validacao: (b) {},
+            ),
+            ZAba(
+              zAbaViewModel: [
+                ZAbaViewModel(
+                    onTap: () {
+                      print("1");
+                    },
+                    titulo: "UF(Estados)"),
+                ZAbaViewModel(
+                    onTap: () {
+                      print("2");
+                    },
+                    titulo: "Feriados"),
+                ZAbaViewModel(
+                    onTap: () {
+                      print("3");
+                    },
+                    titulo: "Submenu 3"),
+                ZAbaViewModel(
+                    onTap: () {
+                      print("4");
+                    },
+                    titulo: "Submenu 4"),
+                ZAbaViewModel(
+                    onTap: () {
+                      print("5");
+                    },
+                    titulo: "Submenu 5")
+              ],
             )
           ],
         ),
