dee145b9adf49b12bb414c89369a993efa656748
Luiz-fss <luiz.fss@hotmail.com>
2021-06-11 15:03:55 -0300

    Inclusão da parte de editar e revogar acessos

--- lib/components/modulo/detalhe-modulo-view.dart
+++ lib/components/modulo/detalhe-modulo-view.dart
@@ -2,19 +2,30 @@ import 'package:brasil_fields/brasil_fields.dart';
 import 'package:flutter/material.dart';
 import 'package:z_components/api/contas/contas-service.dart';
 import 'package:z_components/api/contas/i-contas-service.dart';
+import 'package:z_components/components/filtro/filter-expression.dart';
 import 'package:z_components/components/fluxo-admin/usuarios.dart';
 import 'package:z_components/components/modulo/detalhe-modulo.dart';
 import 'package:z_components/components/utils/dialog-utils.dart';
 import 'package:z_components/components/utils/novo_token.dart';
+import 'package:z_components/components/z-alert-dialog.dart';
+import 'package:z_components/components/z-collection/z-collection-item.dart';
+import 'package:z_components/components/z-collection/z-collection.dart';
+import 'package:z_components/config/z-dialog.dart';
 import 'package:z_components/i-view.dart';
+import 'package:z_components/styles/main-style.dart';
 import 'package:z_components/view-model/app-usuario-conta-viewmodel.dart';
 
 
 class DetalheModuloView extends IView<DetalheModulo>{
   DetalheModuloView(State<DetalheModulo> state) : super(state);
 
+  SearchOptions searchOptions = new SearchOptions();
+  PaginationMetaData paginationMetaData = new PaginationMetaData();
+  ScrollController scrollController;
+  GlobalKey<ZCollectionState> keyPerfil = new GlobalKey<ZCollectionState>();
+
   IContasService contasService;
-  DialogUtils dialogUtils;
+  DialogUtils _dialogUtils;
   TextEditingController dataExpiracaoController = new TextEditingController();
   FocusNode dataExpiracaoFocus = new FocusNode();
   TextEditingController dataController = new TextEditingController();
@@ -28,6 +39,7 @@ class DetalheModuloView extends IView<DetalheModulo>{
   TextEditingController dataVinculoController = new TextEditingController();
   FocusNode dataVinculoFocus = new FocusNode();
 
+  bool alterouPerfil = false;
   bool editarDados = false;
   String titulo = "";
   bool cliqueEditar=false;
@@ -38,7 +50,9 @@ class DetalheModuloView extends IView<DetalheModulo>{
   bool preencheuDataExpiracao=false;
   String textModificar='';
   String textModificarAcesso='';
-
+  String tipoDialog = "";
+  List<String> itensMenu = [];
+  List<ZCollectionItem> listaPerfis = [];
 
 
   @override
@@ -46,19 +60,54 @@ class DetalheModuloView extends IView<DetalheModulo>{
   }
 
   @override
-  Future<void> initView() {
-
-    _preencherDados();
-    dialogUtils = new DialogUtils(state.context);
+  Future<void> initView() async{
+    _dialogUtils = DialogUtils(state.context);
+    _definirTextoDialog();
     contasService = new ContasService(NovoToken.newToken);
     if (state.widget.editarDados) {
-      Future.delayed(Duration(seconds: 1), () {
-        FocusScope.of(state.context).requestFocus(perfilFocus);
-      });
+      itensMenu = [_definirTexto()];
+      listaPerfis = await buscarPerfis(searchOptions);
+      state.setState(() {});
+    } else {
+      itensMenu = ["Editar dados", _definirTexto()];
+    }
+    preencherDados();
+  }
+
+  Future<List<ZCollectionItem>> buscarPerfis(
+      SearchOptions searchOptions) async {
+    var res = await contasService.buscarListaPerfis(
+        searchOptions,state.widget.appUsuarioContaViewModel.app.idApp);
+    List<ZCollectionItem> listaAux = [];
+    if (res != null) {
+      paginationMetaData = res.paginationMetaData;
+      for (int i = 0; i < res.body.length; i++) {
+        listaAux.add(new ZCollectionItem(
+            chave: res.body[i].idPerfil,
+            titulo: res.body[i].nome,
+            chaveSecundaria: res.body[i].idApp,
+            valor: res.body[i].nome));
+      }
+    }
+    return listaAux;
+  }
+
+  String _definirTexto() {
+    if (state.widget.appUsuarioContaViewModel.status == "Ativo") {
+      return "Revogar";
+    } else {
+      return "Ativar";
     }
   }
 
 
+  String _definirTextoDialog() {
+    if (state.widget.appUsuarioContaViewModel.status == "Ativo") {
+      tipoDialog = "removido";
+    } else {
+      tipoDialog = "habilitado";
+    }
+  }
 
   void definirTextoMenu(){
     if(statusController.text=="Ativo"){
@@ -72,9 +121,9 @@ class DetalheModuloView extends IView<DetalheModulo>{
     }
   }
 
-  Widget _preencherDados(){
+  Widget preencherDados(){
     titulo = state.widget.appUsuarioContaViewModel.app.nome;
-    perfilController.text = "Não contém perfil";
+    perfilController.text = state.widget.appUsuarioContaViewModel.perfil.nome ?? "Não contém perfil";
     dataVinculoController.text = _validarDataVinculo();
     dataExpiracaoController.text = _validarDataExpiracao();
     statusController.text = state.widget.appUsuarioContaViewModel.status;
@@ -114,16 +163,26 @@ class DetalheModuloView extends IView<DetalheModulo>{
     if(validar){
       return (){
         if(dataExpiracaoController.text.isEmpty){
-          dialogUtils.showAlertDialogNewAviso("Data inválida", "Por favor, revise a Data obrigatória");
+          _dialogUtils.showAlertDialogNewAviso("Data inválida", "Por favor, revise a Data obrigatória");
           dataExpiracaoFocus.requestFocus();
         }else{
-          print("Válido");
+          state.setState(() {
+            state.widget.appUsuarioContaViewModel.perfil.nome = perfilController.text;
+            print(state.widget.appUsuarioContaViewModel.dataInativacao);
+            print(dataExpiracaoController.text);
+            state.widget.appUsuarioContaViewModel.dataInativacao = _montarData(dataExpiracaoController.text);
+            _alterarAcesso("perfil");
+          });
         }
 
       };
     }else{
       return null;
     }
+  }
+
+  String _montarData(String data){
+    return "${data.split("/")[2]}-${data.split("/")[1]}-${data.split("/")[0]}T00:00:00";
   }
 
   Function cliqueModificarAcesso(AppUsuarioContaViewModel appUsuarioConta){
@@ -158,6 +217,119 @@ class DetalheModuloView extends IView<DetalheModulo>{
       appsFormatados = "Sem apps vinculados";
     }
     return appsFormatados;
+  }
+
+  Future showDialogAlterarAcesso() async {
+    return showDialog(
+        context: state.context,
+        builder: (context) => new ZAlertDialog(
+          zDialog: ZDialog.alert,
+          child: new Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: new Column(
+              children: <Widget>[
+                new Container(
+                  padding: const EdgeInsets.all(4.0),
+                  child: new Text(
+                    "${_definirTexto()} acesso",
+                    style: MainStyle.get(context).mainStyleTextTitle,
+                  ),
+                ),
+                new Container(
+                  child: new Text(
+                    "O acesso do aplicativo: ${state.widget.appUsuarioContaViewModel.app.nomeExibicao} será $tipoDialog para ${state.widget.appUsuarioContaViewModel.usuario.nome}",
+                    style: TextStyle(fontSize: 14),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                new Container(
+                  padding: const EdgeInsets.all(4.0),
+                  child: new Text(
+                    "Deseja prosseguir?",
+                    textAlign: TextAlign.center,
+                    style: new TextStyle(
+                        fontSize: MainStyle.get(context).subTitleFontSize),
+                  ),
+                ),
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    new Container(
+                      child: new InkWell(
+                        borderRadius: new BorderRadius.all(
+                            const Radius.circular(20.0)),
+                        splashColor: const Color(0xffe6e6e6),
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: new Container(
+                          padding: const EdgeInsets.all(12),
+                          child: new Text(
+                            "CANCELAR",
+                            style: new TextStyle(
+                                fontWeight: FontWeight.normal,
+                                color: Color(0xff707070)),
+                          ),
+                        ),
+                      ),
+                      margin: const EdgeInsets.only(bottom: 8),
+                    ),
+                    new Container(
+                      child: new InkWell(
+                        borderRadius: new BorderRadius.all(
+                            const Radius.circular(20.0)),
+                        splashColor: const Color(0xffe6e6e6),
+                        onTap: () async {
+                          if (state
+                              .widget.appUsuarioContaViewModel.status ==
+                              "Ativo") {
+                            state.widget.appUsuarioContaViewModel.status =
+                            "Inativo";
+                          } else {
+                            state.widget.appUsuarioContaViewModel.status =
+                            "Ativo";
+                          }
+                          Navigator.pop(context);
+                          await _alterarAcesso("acesso");
+                        },
+                        child: new Container(
+                          padding: const EdgeInsets.all(12),
+                          child: new Text(
+                            "CONFIRMAR",
+                            style:
+                            new TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      margin: const EdgeInsets.only(bottom: 8),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Future<void> _alterarAcesso(String operacao) async {
+    _dialogUtils.showProgressDialog();
+
+    var res = await contasService.editarDadosFluxoUsuario(
+        state.widget.appUsuarioContaViewModel.app.idModulo,
+        state.widget.appUsuarioContaViewModel.idApp,
+        state.widget.appUsuarioContaViewModel.idUsuario,
+        state.widget.appUsuarioContaViewModel,
+        operacao);
+
+    if (res != null && res) {
+      _dialogUtils.dismiss();
+      Navigator.of(state.context).pop(state.widget.appUsuarioContaViewModel);
+    } else {
+      _dialogUtils.dismiss();
+      _dialogUtils.showAlertDialogErro(
+          "Erro!", "Não foi possivel finalizar a alteração.");
+      print("Erro");
+    }
   }
 
 }
