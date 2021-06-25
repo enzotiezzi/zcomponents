53ce2fbd95e40c7e2f7e536ce4a7d2cccce31e87
Enzo Tiezzi <enzo_tiezzi@hotmail.com>
2021-06-25 11:48:46 -0300

    arrumando z injector

--- lib/components/z-injector/z-injector.dart
+++ lib/components/z-injector/z-injector.dart
@@ -3,10 +3,10 @@ import 'package:injector/injector.dart';
 class ZInjector {
   static void registerDependency<T>(Object instance) {
     if (!Injector.appInstance.exists<T>())
-      Injector.appInstance.registerDependency(() => instance, dependencyName: T.toString());
+      Injector.appInstance.registerDependency(() => instance, dependencyName: T.runtimeType.toString());
   }
 
   static T getDependency<T>() {
-    return Injector.appInstance.get(dependencyName: T.toString());
+    return Injector.appInstance.get(dependencyName: T.runtimeType.toString());
   }
 }
