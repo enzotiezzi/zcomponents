6a5ee5333141e5f81b6b29d47a3ffd739d8d4f27
Giuliano Ortiz <giu.oortiz@gmail.com>
2021-07-28 18:37:22 -0300

    baixar

--- example/lib/tela-teste-card.dart
+++ example/lib/tela-teste-card.dart
@@ -7,6 +7,8 @@ import 'package:z_components/components/z-aba/z-aba-viewmodel.dart';
 import 'package:z_components/components/z-item-tile-os.dart';
 import 'package:z_components/components/z-header.dart';
 import 'package:z_components/components/z-inputs/z-input-cnpj.dart';
+import 'package:z_components/components/z-selection/z-selection.dart';
+import 'package:z_components/components/z-selection/z-selection-item.dart';
 
 class TelaTesteCard extends StatefulWidget {
   @override
@@ -14,6 +16,37 @@ class TelaTesteCard extends StatefulWidget {
 }
 
 class _TelaTesteCardState extends State<TelaTesteCard> {
+  List<ZSelectionItem> listaSelecao = [];
+
+  @override
+  void initState() {
+    listaSelecao = [
+      ZSelectionItem(
+          chave: "Universidade", titulo: "Universidade", valor: "Universidade"),
+      ZSelectionItem(chave: "1", titulo: "1", valor: "1"),
+      ZSelectionItem(chave: "2", titulo: "2", valor: "2"),
+      ZSelectionItem(chave: "3", titulo: "3", valor: "3"),
+      ZSelectionItem(chave: "4", titulo: "4", valor: "4"),
+      ZSelectionItem(chave: "5", titulo: "5", valor: "5"),
+      ZSelectionItem(chave: "6", titulo: "6", valor: "6"),
+      ZSelectionItem(chave: "7", titulo: "7", valor: "7"),
+      ZSelectionItem(chave: "8", titulo: "8", valor: "8"),
+      ZSelectionItem(chave: "9", titulo: "9", valor: "9"),
+      ZSelectionItem(chave: "10", titulo: "10", valor: "10"),
+      ZSelectionItem(chave: "11", titulo: "11", valor: "11"),
+      ZSelectionItem(chave: "12", titulo: "12", valor: "12"),
+      ZSelectionItem(chave: "13", titulo: "13", valor: "13"),
+      ZSelectionItem(chave: "14", titulo: "14", valor: "14"),
+      ZSelectionItem(chave: "15", titulo: "15", valor: "15"),
+      ZSelectionItem(chave: "16", titulo: "16", valor: "16"),
+      ZSelectionItem(chave: "17", titulo: "17", valor: "17"),
+      ZSelectionItem(chave: "18", titulo: "18", valor: "18"),
+      ZSelectionItem(chave: "19", titulo: "19", valor: "19"),
+      ZSelectionItem(chave: "20", titulo: "20", valor: "20"),
+    ];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +62,7 @@ class _TelaTesteCardState extends State<TelaTesteCard> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            ZHeader(
+            /*    ZHeader(
               titulo: "Minhas Ordens de Serviço",
             ),
             Container(
@@ -61,12 +94,12 @@ class _TelaTesteCardState extends State<TelaTesteCard> {
               porque: "Dispenser Papel",
               prioridade: "Baixa",
               dataSla: "",
-              visibilidade:false,
+              visibilidade: false,
               emAndamento: true,
               onPressedFinalizar: () {
                 print("tap no icone");
               },
-              onTap: (){
+              onTap: () {
                 print("tap no card");
               },
             ),
@@ -77,12 +110,12 @@ class _TelaTesteCardState extends State<TelaTesteCard> {
               porque: "Dispenser Papel",
               prioridade: "Baixa",
               dataSla: "",
-              visibilidade:true,
+              visibilidade: true,
               emAndamento: false,
               onPressedFinalizar: () {
                 print("tap no icone");
               },
-              onTap: (){
+              onTap: () {
                 print("tap no card");
               },
             ),
@@ -97,8 +130,18 @@ class _TelaTesteCardState extends State<TelaTesteCard> {
               cnpjFocus: new FocusNode(),
               controllerCNPJ: new TextEditingController(),
               validacao: (b) {},
-            ),
-            ZAba(
+            ),*/
+            new Container(
+              margin: EdgeInsets.only(top: 10),
+              child: ZSelection(
+                  themeData: Theme.of(context),
+                  titulo: "Teste Seleção",
+                  onChange: (value) {
+                    listaSelecao = value;
+                  },
+                  lista: listaSelecao),
+            )
+            /*   ZAba(
               zAbaViewModel: [
                 ZAbaViewModel(
                     icon: Icon(Icons.person),
@@ -112,8 +155,7 @@ class _TelaTesteCardState extends State<TelaTesteCard> {
                     },
                     titulo: "Feriados"),
               ],
-            ),
-
+            ),*/
           ],
         ),
       ),
